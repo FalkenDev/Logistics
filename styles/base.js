a71bac88b8a7e956061f809efec0e4b4ff209a32
“FalkenDev” <kafa21@student.bth.se>
2022-06-13 09:27:34 +0100

    Styles for the project

---
+++ styles/base.js
@@ -0,0 +1,53 @@
+export const container = {
+    flex: 1,
+}
+
+export const base = {
+    flex: 1,
+    backgroundColor: '#1D3557',
+    paddingLeft: 12,
+    paddingRight: 12,
+}
+
+export const viewDelivery = {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 11,
+    marginBottom: 11,
+    paddingLeft: 12,
+    paddingRight: 12,
+    height:100,
+    borderWidth: 1,
+    borderRadius: 10,
+}
+
+export const img = {
+    width: 385,
+    height: 260,
+    alignSelf: 'center', 
+}
+
+export const products = {
+    fontSize: 18,
+    paddingTop: 5,
+}
+
+export const details_button = {
+    marginLeft: 0,
+    width: 100,
+    color: '#211',
+    marginTop: 100,
+}
+
+export const input = {
+    fontSize: 20,
+    marginBottom: 28,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "#ccc",
+    borderRadius: 3,
+};
+
+export const dateButton = {
+    color: "#211 !important",
+};
