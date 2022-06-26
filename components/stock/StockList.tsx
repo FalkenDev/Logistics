b9dbda1655b08ae1f52af4c4019240d567e57386
“FalkenDev” <kafa21@student.bth.se>
2022-06-26 12:20:45 +0100

    Changed to use DataTable with button press

--- components/stock/StockList.tsx
+++ components/stock/StockList.tsx
@@ -1,10 +1,11 @@
 import { useCallback, useEffect, useState } from 'react';
 import { ImageBackground, ScrollView, Text, Button, StyleSheet, Pressable, View } from "react-native";
-import { Typography, Base } from '../../styles/index.js';
+import { Typography, Base, StockStyle, DashboardStyle } from '../../styles/index.js';
 import productModel from "../../models/product";
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import SearchBar from "react-native-dynamic-search-bar";
+import { DataTable } from 'react-native-paper';
 
 export default function StockList({ route, navigation, products, setProducts }) {
     console.log("------| Loading StockList from Stock |------")
@@ -13,10 +14,11 @@ export default function StockList({ route, navigation, products, setProducts }) 
     useEffect(() => {
         (async () => {
             setProducts(await productModel.getProducts());
+            setSearchProducts(products);
         })();
     }, []);
 
-    let listOfSearchProducts = products;
+    let listOfSearchProducts;
 
     async function searchFunction(name: string) {
         console.log(name);
@@ -29,28 +31,32 @@ export default function StockList({ route, navigation, products, setProducts }) 
 
     return (
         <ScrollView>
-            <SearchBar
+            <SearchBar style={{marginTop: 20}}
                 placeholder="Search here"
                 onPress={() => setSearchProducts(products)}
                 onChangeText={(text) => searchFunction(text)}
             />
+            <DataTable>
+                <DataTable.Header>
+                    <DataTable.Title style={{flex: 1.3}}>#</DataTable.Title>
+                    <DataTable.Title style={{flex: 1.3}}>ArticleNr</DataTable.Title>
+                    <DataTable.Title style={{flex: 1.8}}>Name</DataTable.Title>
+                    <DataTable.Title style={{flex: 1}}>Stock</DataTable.Title>
+                    <DataTable.Title style={{flex: 0.4}}></DataTable.Title>
+                </DataTable.Header>
+            </DataTable>
             { listOfSearchProducts = searchProducts
             .map((item, index) => {
                 return (
-                <Pressable onPress={() => { navigation.navigate('Details', {item: item}); }}>
-                    <View style={styles.container}>
-                        <View style={styles.stockContainerLogo}>
-                            <View style={styles.circle}><FontAwesome5 style={styles.logo} name="box" size={40} color="white" /></View>
-                        </View>
-                        <View style={styles.stockContainerText}>
-                            <Text style={styles.textInfo}>{item.name}</Text>
-                            <Text style={styles.textInfo}>{item.stock}</Text>
-                        </View>
-                        <View style={styles.stockContainerArrow}>
-                            <AntDesign name="right" size={24} color="white" />
-                        </View>
-                    </View>
-                </Pressable>
+                    <Pressable key={index} onPress={() => { navigation.navigate('Details', {item: item}); }}>
+                        <DataTable.Row style={{height: 90}}>
+                            <DataTable.Cell style={{flex: 1.3}}><View style={DashboardStyle.circle}><FontAwesome5 style={DashboardStyle.logo} name="box" size={30} color="white" /></View></DataTable.Cell>
+                            <DataTable.Cell style={{flex: 1.3}}>{item.article_number}</DataTable.Cell>
+                            <DataTable.Cell style={{flex: 1.8}}>{item.name}</DataTable.Cell>
+                            <DataTable.Cell style={{flex: 1}}>{item.stock}</DataTable.Cell>
+                            <DataTable.Cell style={{flex: 0.4}}><MaterialIcons name="arrow-forward-ios" size={14} color="black" /></DataTable.Cell>
+                        </DataTable.Row>
+                    </Pressable>
                 )
             })
             }
@@ -58,59 +64,3 @@ export default function StockList({ route, navigation, products, setProducts }) 
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "flex-end",
-        flexDirection: "row",
-        alignItems: "center",
-        marginTop: 20,
-        lineHeight: 20,
-        backgroundColor: "#A8DADC",
-        borderRadius: 20,
-        height: 100,
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 4,
-        },
-        shadowOpacity: 0.30,
-        shadowRadius: 4.65,
-  
-        elevation: 8,
-    },
-    circle: {
-        width: 80,
-        height: 80,
-        borderRadius: 100,
-        backgroundColor: "#457B9D",
-        marginLeft: "auto",
-        marginRight: "auto",
-    },
-    logo: {
-      marginLeft: "auto",
-      marginRight: "auto",
-      marginTop: "auto",
-      marginBottom: "auto",
-    },
-    stockContainerLogo: {
-      width: "30%"
-    },
-    stockContainerText: {
-      width: "60%"
-    },
-    stockContainerArrow: {
-        width: "10%"
-    },
-    textNumber: {
-        fontSize: 34,
-        fontWeight: 'bold',
-        letterSpacing: 0.25,
-        color: 'black',
-    },
-    textInfo: {
-      fontSize: 16,
-      letterSpacing: 0.25,
-      color: 'black',
-    }
-  });
