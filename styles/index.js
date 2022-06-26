bfc7bd4ca86f30414447676034e4236bdc0142eb
“FalkenDev” <kafa21@student.bth.se>
2022-06-26 12:17:33 +0100

    Small style fixes

--- styles/index.js
+++ styles/index.js
@@ -1,5 +1,7 @@
 import * as Base from './base';
 import * as Typography from './typography';
 import * as Forms from './forms';
+import * as DashboardStyle from './dashboard';
+import * as StockStyle from './stock';
 
-export { Base, Typography, Forms };
+export { Base, Typography, Forms, DashboardStyle, StockStyle };
