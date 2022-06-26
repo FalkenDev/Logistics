17e9ac70a7f8f9a02a26b0e0bc028cc31f0bff99
“FalkenDev” <kafa21@student.bth.se>
2022-06-26 12:18:53 +0100

    Css Style for Stock screen

---
+++ styles/stock.js
@@ -0,0 +1,61 @@
+export const container = {
+    flex: 1,
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 20,
+    lineHeight: 20,
+    backgroundColor: "#A8DADC",
+    borderRadius: 20,
+    height: 100,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 4.65,
+
+    elevation: 8,
+};
+
+export const circle = {
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    backgroundColor: "#457B9D",
+    marginLeft: "auto",
+    marginRight: "auto",
+};
+
+export const logo = {
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "auto",
+    marginBottom: "auto",
+};
+
+export const stockContainerLogo = {
+    width: "30%"
+};
+
+export const stockContainerText = {
+    width: "60%"
+};
+
+export const stockContainerArrow = {
+    width: "10%"
+};
+
+export const textNumber = {
+    fontSize: 34,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+};
+
+export const textInfo = {
+    fontSize: 16,
+    letterSpacing: 0.25,
+    color: 'black',
+};
