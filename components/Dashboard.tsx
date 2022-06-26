cde8022b1f802d0d9f8bd4b3fce966b66b112f09
“FalkenDev” <kafa21@student.bth.se>
2022-06-26 12:17:02 +0100

    Added image and fixed style on the counters

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,56 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, Text, ScrollView, View, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Base, Typography } from '../styles/index.js';
+import { Base, Typography, DashboardStyle } from '../styles/index.js';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons'; 
+import backgroundImage from "../assets/androidback.png";
 
 export default function Home({ route, delivery, orders }) {
 
-  const deliveryLength = delivery.length || 0;
+  const deliverysLength = delivery.length || 0;
   const ordersLength = orders.length || 0;
-
-  let count = 0;
-
-  orders
-  .filter(order => order.status === "Skickad")
-  .map((order, index) => {
-      count = count + 1
-  });
+  const submissionsLength = 0;
 
   return (
     <SafeAreaView style={Base.container}>
       <ScrollView style={Base.base}>
         <View></View>
-
-        <View style={styles.container}>
-          <View style={styles.dashContainerLogo}>
-            <View style={styles.circle}><FontAwesome5 style={styles.logo} name="box" size={40} color="white" /></View>
+        <View style={DashboardStyle.imageContainer}>
+          <Image source={backgroundImage} style={DashboardStyle.imageDisplay}></Image>
+        </View>
+        <View style={DashboardStyle.container}>
+          <View style={DashboardStyle.dashContainerLogo}>
+            <View style={DashboardStyle.circle}><FontAwesome5 style={DashboardStyle.logo} name="box" size={30} color="white" /></View>
           </View>
-          <View style={styles.dashContainerText}>
-            <Text style={styles.textNumber}>{ordersLength}</Text>
-            <Text style={styles.textInfo}>Orders in progress</Text>
+          <View style={DashboardStyle.dashContainerText}>
+            <Text style={DashboardStyle.textNumber}>{ordersLength}</Text>
+            {ordersLength === 1
+              ?<Text style={DashboardStyle.textInfo}>Order in progress</Text>
+              :<Text style={DashboardStyle.textInfo}>Orders in progress</Text>
+            }
           </View>
         </View>
 
-        <View style={styles.container}>
-          <View style={styles.dashContainerLogo}>
-            <View style={styles.circle}><MaterialCommunityIcons style={styles.logo} name="truck-delivery" size={48} color="white" /></View>
+        <View style={DashboardStyle.container}>
+          <View style={DashboardStyle.dashContainerLogo}>
+            <View style={DashboardStyle.circle}><MaterialCommunityIcons style={DashboardStyle.logo} name="truck-delivery" size={38} color="white" /></View>
           </View>
-          <View style={styles.dashContainerText}>
-            <Text style={styles.textNumber}>{count}</Text>
-            <Text style={styles.textInfo}>Deliverys in Progress</Text>
+          <View style={DashboardStyle.dashContainerText}>
+            <Text style={DashboardStyle.textNumber}>{deliverysLength}</Text>
+            {deliverysLength === 1
+              ?<Text style={DashboardStyle.textInfo}>Delivery in Progress</Text>
+              :<Text style={DashboardStyle.textInfo}>Deliverys in Progress</Text>
+            }
           </View>
         </View>
 
-        <View style={styles.container}>
-          <View style={styles.dashContainerLogo}>
-            <View style={styles.circle}><MaterialIcons style={styles.logo} name="add-circle" size={54} color="white" /></View>
+        <View style={DashboardStyle.container}>
+          <View style={DashboardStyle.dashContainerLogo}>
+            <View style={DashboardStyle.circle}><MaterialIcons style={DashboardStyle.logo} name="add-circle" size={34} color="white" /></View>
           </View>
-          <View style={styles.dashContainerText}>
-            <Text style={styles.textNumber}>{deliveryLength}</Text>
-            <Text style={styles.textInfo}>Submissions</Text>
+          <View style={DashboardStyle.dashContainerText}>
+            <Text style={DashboardStyle.textNumber}>{submissionsLength}</Text>
+            {submissionsLength === 1
+              ?<Text style={DashboardStyle.textInfo}>Submission</Text>
+              :<Text style={DashboardStyle.textInfo}>Submissions</Text>
+            }
           </View>
         </View>
 
@@ -59,57 +64,3 @@ export default function Home({ route, delivery, orders }) {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      justifyContent: "flex-end",
-      flexDirection: "row",
-      alignItems: "center",
-      marginTop: 20,
-      lineHeight: 20,
-      backgroundColor: "#A8DADC",
-      borderRadius: 20,
-      height: 100,
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 4,
-      },
-      shadowOpacity: 0.30,
-      shadowRadius: 4.65,
-
-      elevation: 8,
-  },
-  circle: {
-      width: 80,
-      height: 80,
-      borderRadius: 100,
-      backgroundColor: "#457B9D",
-      marginLeft: "auto",
-      marginRight: "auto",
-  },
-  logo: {
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginTop: "auto",
-    marginBottom: "auto",
-  },
-  dashContainerLogo: {
-    width: "30%"
-  },
-  dashContainerText: {
-    width: "70%"
-  },
-  textNumber: {
-      fontSize: 34,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'black',
-  },
-  textInfo: {
-    fontSize: 16,
-    letterSpacing: 0.25,
-    color: 'black',
-  }
-});
