dc810d39a4ec147ae33933170f104944dea889e6
“FalkenDev” <kafa21@student.bth.se>
2022-06-26 12:19:15 +0100

    Css style for Dashboard screen

---
+++ styles/dashboard.js
@@ -0,0 +1,70 @@
+export const container = {
+    flex: 1,
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 15,
+    lineHeight: 10,
+    backgroundColor: "#040C33",
+    borderRadius: 20,
+    height: 80,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 4.65,
+
+    elevation: 8,
+};
+
+export const circle = {
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    backgroundColor: "#457B9D",
+    marginLeft: "auto",
+    marginRight: "auto",
+};
+
+export const logo = {
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "auto",
+    marginBottom: "auto",
+};
+
+export const dashContainerLogo = {
+    width: "30%"
+};
+
+export const dashContainerText = {
+    width: "70%"
+};
+
+export const textNumber = {
+    fontSize: 30,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+};
+
+export const textInfo = {
+    fontSize: 16,
+    letterSpacing: 0.25,
+    color: 'white',
+    marginBottom: 6,
+};
+
+export const imageDisplay = {
+    width: 400,
+    height: 200,
+    marginBottom: -20,
+}
+
+
+export const imageContainer = {
+    justifyContent: "center",
+    alignItems: "center",
+}
