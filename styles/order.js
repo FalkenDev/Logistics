0221426eb3a83e98538152cb3ddcc7406ecda853
“FalkenDev” <kafa21@student.bth.se>
2022-07-06 22:57:30 +0100

    Style for order stackscreen

---
+++ styles/order.js
@@ -0,0 +1,84 @@
+export const container = {
+    flex: 1,
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 20,
+    lineHeight: 20,
+    backgroundColor: "#040C33",
+    borderRadius: 20,
+    height: 80,
+    width: 350,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 4.65,
+
+    elevation: 8,
+};
+
+export const statusBar = {
+    width: 70,
+    height: 22,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 36,
+    borderRadius: 10
+};
+
+export const statusBarText = {
+    marginLeft: "auto",
+    marginRight: "auto",
+    textAlign: "center",
+    justifyContent: "center",
+    textAlignVertical: "center"
+}
+
+export const rectangle = {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FF9393",
+    marginLeft: "auto",
+    marginRight: "auto",
+};
+
+export const circle = {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+    backgroundColor: "#FF9393",
+    marginLeft: "auto",
+    marginRight: "auto",
+};
+
+export const logo = {
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "auto",
+    marginBottom: "auto",
+};
+
+export const stockContainerLogo = {
+    width: "30%"
+};
+
+export const stockContainerText = {
+    width: "70%"
+};
+
+export const textNumber = {
+    fontSize: 30,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+};
+
+export const textInfo = {
+    fontSize: 16,
+    letterSpacing: 0.25,
+    color: 'white',
+    marginBottom: 7,
+};
