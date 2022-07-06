870adf2fd9b3e506fee9ae74e82e4d3d90feb592
“FalkenDev” <kafa21@student.bth.se>
2022-07-06 21:16:37 +0100

    Started to work on details view

--- components/stock/Product.tsx
+++ components/stock/Product.tsx
@@ -1,3 +1,32 @@
-export default function InfoList({ route, navigation }) {
-    return
+import { View, Text, Pressable, Image } from "react-native";
+import { useState, useEffect } from 'react';
+import { Base, Typography,DashboardStyle } from '../../styles/index.js';
+import backgroundImage from "../../assets/androidback.png"; //Byt ut
+
+export default function InfoList({ route, navigation, setProducts }) {
+    console.log("Product Info");
+    const { item } = route.params;
+
+    return (
+        <View>
+            <View>
+                <Image source={backgroundImage} style={DashboardStyle.imageDisplay}></Image>
+            </View>
+            <View>
+                <Text style={Typography.details_text}>{item.id}</Text>
+                <Text style={Typography.details_text}>{item.article_number}</Text>
+                <Text style={Typography.details_text}>{item.name}</Text>
+                <Text style={Typography.details_text}>{item.description ?? "None"}</Text>
+                <Text style={Typography.details_text}>{item.specifiersc ?? "None"}</Text>
+                <Text style={Typography.details_text}>{item.stock}</Text>
+                <Text style={Typography.details_text}>{item.location}</Text>
+                <Text style={Typography.details_text}>{item.price}</Text>
+            </View>
+            <View>
+                <Pressable><Text>Edit</Text></Pressable>
+                <Pressable><Text>Remove</Text></Pressable>
+            </View>
+        </View>
+    )
+    
 }
