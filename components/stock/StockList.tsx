4d2713a0e1cb0c48d6a4c9ebbd969fc2595a1e0e
“FalkenDev” <kafa21@student.bth.se>
2022-07-06 21:17:32 +0100

    Style fixes

--- components/stock/StockList.tsx
+++ components/stock/StockList.tsx
@@ -9,6 +9,7 @@ import { DataTable } from 'react-native-paper';
 
 export default function StockList({ route, navigation, products, setProducts }) {
     console.log("------| Loading StockList from Stock |------")
+    let productLength = products.length;
     const [searchProducts, setSearchProducts] = useState([]);
 
     useEffect(() => {
@@ -30,12 +31,26 @@ export default function StockList({ route, navigation, products, setProducts }) 
     }
 
     return (
-        <ScrollView>
-            <SearchBar style={{marginTop: 20}}
+        <ScrollView style={{backgroundColor: "white"}}>
+            <SearchBar style={{marginTop: 20, borderBottomColor: "grey", borderBottomWidth: 1}}
                 placeholder="Search here"
                 onPress={() => setSearchProducts(products)}
                 onChangeText={(text) => searchFunction(text)}
             />
+            <View style={{marginLeft: "auto", marginRight: "auto"}}>
+                <View style={StockStyle.container}>
+                    <View style={StockStyle.stockContainerLogo}>
+                        <View style={StockStyle.circle}><FontAwesome5 style={DashboardStyle.logo} name="box" size={28} color="white" /></View>
+                    </View>
+                    <View style={StockStyle.stockContainerText}>
+                        <Text style={StockStyle.textNumber}>{productLength}</Text>
+                        {productLength === 1
+                        ?<Text style={StockStyle.textInfo}>Item in Stock</Text>
+                        :<Text style={StockStyle.textInfo}>Items in Stock</Text>
+                        }
+                    </View>
+                </View>
+            </View>
             <DataTable>
                 <DataTable.Header>
                     <DataTable.Title style={{flex: 1.3}}>#</DataTable.Title>
@@ -50,7 +65,7 @@ export default function StockList({ route, navigation, products, setProducts }) 
                 return (
                     <Pressable key={index} onPress={() => { navigation.navigate('Details', {item: item}); }}>
                         <DataTable.Row style={{height: 90}}>
-                            <DataTable.Cell style={{flex: 1.3}}><View style={DashboardStyle.circle}><FontAwesome5 style={DashboardStyle.logo} name="box" size={30} color="white" /></View></DataTable.Cell>
+                            <DataTable.Cell style={{flex: 1.4}}><View style={StockStyle.rectangle}><FontAwesome5 style={DashboardStyle.logo} name="box" size={30} color="white" /></View></DataTable.Cell>
                             <DataTable.Cell style={{flex: 1.3}}>{item.article_number}</DataTable.Cell>
                             <DataTable.Cell style={{flex: 1.8}}>{item.name}</DataTable.Cell>
                             <DataTable.Cell style={{flex: 1}}>{item.stock}</DataTable.Cell>
