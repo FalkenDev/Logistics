1707dd1f4f27bf0e78fa887fb04d149b0f7dccea
“FalkenDev” <kafa21@student.bth.se>
2022-07-11 20:24:07 +0100

    Added css for details screen

--- styles/stock.js
+++ styles/stock.js
@@ -65,3 +65,62 @@ export const textInfo = {
     color: 'white',
     marginBottom: 7,
 };
+
+export const details_header = {
+    backgroundColor: "#FF9393",
+    height: 140,
+    marginBottom: 10,
+}
+
+export const details_text_title = {
+    fontSize: 18,
+    marginLeft: 11,
+    marginTop: 5,
+    marginBottom: 5,
+    color: "#818181",
+};
+
+export const details_text_output = {
+    fontSize: 18,
+    marginLeft: 11,
+    marginBottom: 5,
+};
+
+export const details_text_id = {
+    color: "#003EE1",
+}
+
+export const details_line = {
+    height: 2,
+    backgroundColor: "#BDBDBD",
+    width: "94%",
+    alignSelf: "center",
+    marginTop: 5,
+    marginBottom: 5
+};
+
+export const details_button_edit = {
+    width: "94%",
+    height: 40,
+    backgroundColor: "#FF8F8F",
+    borderRadius: 5,
+    alignSelf: "center",
+    marginTop: 10,
+    marginBottom: 10
+};
+
+export const details_button_remove = {
+    width: "94%",
+    height: 40,
+    backgroundColor: "#FF4E4E",
+    borderRadius: 5,
+    alignSelf: "center",
+    marginTop: 10,
+    marginBottom: 10
+};
+
+export const details_button_text = {
+    margin: "auto",
+    alignSelf: "center",
+    fontSize: 25
+}
