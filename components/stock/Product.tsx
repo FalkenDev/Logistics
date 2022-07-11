40166d6f97929084d1d849835b633fcaab3d43a0
“FalkenDev” <kafa21@student.bth.se>
2022-07-11 20:23:38 +0100

    Edited style for Product detail

--- components/stock/Product.tsx
+++ components/stock/Product.tsx
@@ -1,32 +1,61 @@
-import { View, Text, Pressable, Image } from "react-native";
+import { View, Text, Pressable, Image, ScrollView } from "react-native";
 import { useState, useEffect } from 'react';
-import { Base, Typography,DashboardStyle } from '../../styles/index.js';
+import { Base, Typography,DashboardStyle, StockStyle } from '../../styles/index.js';
 import backgroundImage from "../../assets/androidback.png"; //Byt ut
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
+import { header2 } from "../../styles/typography.js";
+import Stock from "./Stock.js";
 
 export default function InfoList({ route, navigation, setProducts }) {
     console.log("Product Info");
     const { item } = route.params;
 
     return (
-        <View>
-            <View>
-                <Image source={backgroundImage} style={DashboardStyle.imageDisplay}></Image>
+        <ScrollView>
+            <View style={StockStyle.details_header}>
+                <FontAwesome5 style={DashboardStyle.logo} name="box" size={60} color="white" />
             </View>
             <View>
-                <Text style={Typography.details_text}>{item.id}</Text>
-                <Text style={Typography.details_text}>{item.article_number}</Text>
-                <Text style={Typography.details_text}>{item.name}</Text>
-                <Text style={Typography.details_text}>{item.description ?? "None"}</Text>
-                <Text style={Typography.details_text}>{item.specifiersc ?? "None"}</Text>
-                <Text style={Typography.details_text}>{item.stock}</Text>
-                <Text style={Typography.details_text}>{item.location}</Text>
-                <Text style={Typography.details_text}>{item.price}</Text>
+                <Text style={StockStyle.details_text_title}>Name</Text>
+                <Text style={StockStyle.details_text_output}>{item.name}</Text>
+                <View style={StockStyle.details_line}></View>
+
+                <Text style={StockStyle.details_text_title}>ID</Text>
+                <Text style={StockStyle.details_text_output}><Text style={StockStyle.details_text_id}>#{item.id}</Text></Text>
+                <View style={StockStyle.details_line}></View>
+
+                <Text style={StockStyle.details_text_title}>Article number</Text>
+                <Text style={StockStyle.details_text_output}>{item.article_number}</Text>
+                <View style={StockStyle.details_line}></View>
+
+                <Text style={StockStyle.details_text_title}>Description</Text>
+                <Text style={StockStyle.details_text_output}>{item.description ?? "None"}</Text>
+                <View style={StockStyle.details_line}></View>
+
+                <Text style={StockStyle.details_text_title}>Specifiers</Text>
+                <Text style={StockStyle.details_text_output}>{item.specifiers ?? "None"}</Text>
+                <View style={StockStyle.details_line}></View>
+
+                <Text style={StockStyle.details_text_title}>Stock</Text>
+                <Text style={StockStyle.details_text_output}>{item.stock}</Text>
+                <View style={StockStyle.details_line}></View>
+
+                <Text style={StockStyle.details_text_title}>Location</Text>
+                <Text style={StockStyle.details_text_output}>{item.location}</Text>
+                <View style={StockStyle.details_line}></View>
+
+                <Text style={StockStyle.details_text_title}>Price</Text>
+                <Text style={StockStyle.details_text_output}>${item.price}</Text>
             </View>
             <View>
-                <Pressable><Text>Edit</Text></Pressable>
-                <Pressable><Text>Remove</Text></Pressable>
+                <Pressable style={StockStyle.details_button_edit}>
+                    <Text style={StockStyle.details_button_text}>EDIT</Text>
+                </Pressable>
+                <Pressable style={StockStyle.details_button_remove}>
+                    <Text style={StockStyle.details_button_text}>Remove Item</Text>
+                </Pressable>
             </View>
-        </View>
+        </ScrollView>
     )
     
 }
