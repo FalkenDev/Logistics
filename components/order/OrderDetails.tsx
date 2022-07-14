b46c671b9379588f32760bb96a5002a9f1b7115a
“FalkenDev” <kafa21@student.bth.se>
2022-07-14 22:21:13 +0100

    Fixed orderStatusFunction to work on the status loading.

--- components/order/OrderDetails.tsx
+++ components/order/OrderDetails.tsx
@@ -4,39 +4,17 @@ import { Base, Typography, DashboardStyle, StockStyle } from '../../styles/index
 import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
 import backgroundImage from "../../assets/androidback.png"; //Byt ut
 import { DataTable } from 'react-native-paper';
+import orderModel from '../../models/orders';
 
 export default function InfoList({ route, navigation, setProducts }) {
     console.log("Product Info");
     const { order } = route.params;
     let orderItems;
-    let statusBackgroundColor;
-    let statusName;
 
+    let orderStatusArray = orderModel.orderStatusFunction(order.status_id);
+    let statusBackgroundColor = orderStatusArray[0];
+    let statusName = orderStatusArray[1];
 
-    async function orderStatusFunction(orderStatus: number) {
-        console.log(orderStatus);
-        if(orderStatus === 100) /* New */ {
-            statusBackgroundColor = '#FCFF51';
-            statusName = 'Accepted';
-        } else if(orderStatus === 200) /* Packed */ {
-            statusBackgroundColor = '#FF636C';
-            statusName = 'Packed';
-        } else if(orderStatus === 400) /* Sent */ {
-            statusBackgroundColor = '#44FF4B';
-            statusName = 'Sent';
-        } else if(orderStatus === 600) /* Invoiced */ {
-            statusBackgroundColor = '#6951FF';
-            statusName = 'Invoiced';
-        } else if(orderStatus === 800) /* Return */ {
-            statusBackgroundColor = '#49FFFF';
-            statusName = 'Return';
-        } else if(orderStatus === 900) /* Refunded */ {
-            statusBackgroundColor = '#FF80DB';
-            statusName = 'Refunded';
-        }
-    }
-
-    orderStatusFunction(order.status_id);
 
     return (
         <ScrollView style={{backgroundColor: "white"}}>
@@ -44,10 +22,22 @@ export default function InfoList({ route, navigation, setProducts }) {
                 <FontAwesome5 style={DashboardStyle.logo} name="box" size={60} color="white" />
             </View>
             <View style={{flex: 1, flexDirection: "row", height: 15}}>
-                <View style={{width: "25%", backgroundColor: "#30FF1E", borderWidth: 2, borderColor: "white"}}></View>
-                <View style={{width: "25%", backgroundColor: "#20241F", borderWidth: 2, borderColor: "white"}}></View>
-                <View style={{width: "25%", backgroundColor: "#20241F", borderWidth: 2, borderColor: "white"}}></View>
-                <View style={{width: "25%", backgroundColor: "#20241F", borderWidth: 2, borderColor: "white"}}></View>
+                {order.status_id >= 100
+                    ?<View style={{width: "25%", backgroundColor: "#30FF1E", borderWidth: 2, borderColor: "white"}}></View>
+                    :<View style={{width: "25%", backgroundColor: "#20241F", borderWidth: 2, borderColor: "white"}}></View>
+                }
+                {order.status_id >= 200
+                    ?<View style={{width: "25%", backgroundColor: "#30FF1E", borderWidth: 2, borderColor: "white"}}></View>
+                    :<View style={{width: "25%", backgroundColor: "#20241F", borderWidth: 2, borderColor: "white"}}></View>
+                }
+                {order.status_id >= 400
+                    ?<View style={{width: "25%", backgroundColor: "#30FF1E", borderWidth: 2, borderColor: "white"}}></View>
+                    :<View style={{width: "25%", backgroundColor: "#20241F", borderWidth: 2, borderColor: "white"}}></View>
+                }
+                {order.status_id >= 600
+                    ?<View style={{width: "25%", backgroundColor: "#30FF1E", borderWidth: 2, borderColor: "white"}}></View>
+                    :<View style={{width: "25%", backgroundColor: "#20241F", borderWidth: 2, borderColor: "white"}}></View>
+                }
             </View>
             <View style={{flex: 1, flexDirection: "row", marginBottom: 20}}>
                 <Text style={{width: "25%", textAlign: "center"}}>Accepted</Text>
@@ -78,15 +68,6 @@ export default function InfoList({ route, navigation, setProducts }) {
 
                 <Text style={StockStyle.details_text_title}>Country</Text>
                 <Text style={StockStyle.details_text_output}>{order.country ?? "None"}</Text>
-                <View style={StockStyle.details_line}></View>
-
-                <Text style={StockStyle.details_text_title}>Status</Text>
-                <Text style={StockStyle.details_text_output}>{statusName}</Text>
-            </View>
-            <View>
-                <Pressable style={StockStyle.details_button_edit}>
-                    <Text style={StockStyle.details_button_text}>Orders is ready to be picked</Text>
-                </Pressable>
             </View>
             <View>
                 <Text>Items:</Text>
@@ -114,7 +95,10 @@ export default function InfoList({ route, navigation, setProducts }) {
                 })}
             </View>
             <View style={{display: "flex"}}>
-                <Pressable style={StockStyle.details_button_edit}><Text style={StockStyle.details_button_text}>Pick</Text></Pressable>
+                {order.status_id < 200
+                    ?<Pressable style={StockStyle.details_button_edit}><Text style={StockStyle.details_button_text}>Pick</Text></Pressable>
+                    :<Text></Text>
+                }
                 <Pressable style={StockStyle.details_button_edit}><Text style={StockStyle.details_button_text}>Cancel order</Text></Pressable>
             </View>
         </ScrollView>
