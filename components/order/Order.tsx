ab570ed72938487bba9f26846ae66fc4aef2ef4c
“FalkenDev” <kafa21@student.bth.se>
2022-07-17 16:40:11 +0100

    Lifted setProducts to Pick StackScreen

--- components/order/Order.tsx
+++ components/order/Order.tsx
@@ -1,12 +1,13 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OrderList from './OrderList';
 import OrderDetails from './OrderDetails';
+import Pick from './Pick';
 
 const Stack = createNativeStackNavigator();
 
 export default function Order(props) {
     return (
-        <Stack.Navigator initialRouteName="Stock">
+        <Stack.Navigator initialRouteName="List">
             <Stack.Screen name="List" options={{headerStyle: {backgroundColor: 'white'}, headerTintColor: 'black', headerTitleStyle: {fontWeight: 'bold'}}}>
                 {(screenProps) => <OrderList {...screenProps}
                 orders={props.orders} setOrders={props.setOrders}/>}
@@ -16,8 +17,8 @@ export default function Order(props) {
                 setOrders={props.setOrders}/>}
             </Stack.Screen>
             <Stack.Screen name="Pick" options={{headerStyle: {backgroundColor: 'white'}, headerTintColor: 'black', headerTitleStyle: {fontWeight: 'bold'}}}>
-                {(screenProps) => <OrderList {...screenProps}
-                orders={props.orders} setOrders={props.setOrders}/>}
+                {(screenProps) => <Pick {...screenProps}
+                setProducts={props.setProducts}/>}
             </Stack.Screen>
             <Stack.Screen name="Cancel" options={{headerStyle: {backgroundColor: 'white'}, headerTintColor: 'black', headerTitleStyle: {fontWeight: 'bold'}}}>
                 {(screenProps) => <OrderList {...screenProps}
